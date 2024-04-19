44aab5814d456e269a6e2f3e39f0a17dcc303e62
Sriram212 <sriramnutulapati@gmail.com>
2024-04-19 04:34:40 -0500

    latest changes

--- Node.java
+++ Node.java
@@ -1,13 +1,24 @@
+/* You may use and modify this interface file for your Assignment 7 */
+/* You may add new methods or change any of the methods in this interface. */
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Node extends Remote {
-    boolean acquireJoinLock(String url) throws RemoteException;
-    void releaseJoinLock(String url) throws RemoteException;
-    String findSuccessor(int key, boolean traceFlag) throws RemoteException;
-    String findPredecessor(int key, boolean traceFlag) throws RemoteException;
-    void insert(String word, String definition) throws RemoteException;
-    String lookup(String word) throws RemoteException;
-    void printFingerTable() throws RemoteException;
-    void printDictionary() throws RemoteException;
+    public void setPredecessor(String predecessor) throws RemoteException;
+    public String  findSuccessor (int key, boolean traceFlag) throws RemoteException;
+    public String  findPredecessor (int key, boolean traceFlag) throws RemoteException;
+    public String  closestPrecedingFinger (int key) throws RemoteException;
+    public String  successor () throws RemoteException;
+    public String  predecessor  () throws RemoteException;
+    public boolean acquireJoinLock (String nodeURL) throws RemoteException;
+    public boolean releaseJoinLock (String nodeURL) throws RemoteException;
+    public boolean insert (String word, String definition) throws RemoteException;
+    public String  lookup (String word) throws RemoteException;
+    public String  printFingerTable() throws RemoteException;
+    public String  printDictionary() throws RemoteException;
+    public void update_finger_table(String s, int i) throws RemoteException;
+    public void join() throws RemoteException;
+    void shutdown() throws RemoteException;
 }
+
